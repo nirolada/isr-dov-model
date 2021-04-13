2b1bbeaab5fdbe4e2bc98a50a13290d3bad924c8
Nir Albo <niralbo@gmail.com>
2021-04-13 10:09:46 +0300

    tests WIP

--- tests/test_geo_objects.py
+++ tests/test_geo_objects.py
@@ -1,14 +1,15 @@
 import unittest
+from collections import namedtuple
 from geo_objects import (
-    ECEFPoint, Point2D, Point3D, GeodeticPoint
+    ECEFPoint, Point2D, Point3D, GeodeticPoint, UTMPoint
 )
 
 
 class TestPoint3D(unittest.TestCase):
     def test_valid(self):
-        vals = 1, 2.5, 3
-        p = Point3D(*vals)
-        self.assertTupleEqual(p.tup, vals)
+        valid = 1, 2.5, 3
+        p = Point3D(*valid)
+        self.assertTupleEqual(p.tup, valid)
     
     def test_invalid(self):
         with self.assertRaises(TypeError):
@@ -16,33 +17,37 @@ class TestPoint3D(unittest.TestCase):
 
 
 class TestGeodeticPoint(unittest.TestCase):
+    Point = namedtuple('Point', ['lon', 'lat', 'h'])
+    valid = Point(0.602138592, 0.593411946, 156.755)
+    
     def test_valid(self):
-        lon, lat, h = 0.602138592, 0.593411946, 156.755
-        p = GeodeticPoint(lon, lat, h)
-        self.assertEqual(p.tup, (lon, lat, h))
+        p = GeodeticPoint(*self.valid)
+        self.assertEqual(p.tup, self.valid)
     
     def test_invalid(self):
         with self.assertRaises(TypeError):
-            GeodeticPoint('a', (3,), 360.5)
+            GeodeticPoint('a', (3,), self.valid.h)
             
     def test_invalid_lon(self):
         with self.assertRaises(ValueError):
-            GeodeticPoint(-4, 0.593411946, 156.755)
+            GeodeticPoint(-4, self.valid.lat, self.valid.h)
     
     def test_invalid_lat(self):
         with self.assertRaises(ValueError):
-            GeodeticPoint(0.602138592, 2, 156.755)
+            GeodeticPoint(self.valid.lon, 2, self.valid.h)
 
 
 class TestECEFPoint(unittest.TestCase):
+    Point = namedtuple('Point', ['x', 'y', 'z'])
+    valid = Point(4449038.5, 3094664, 3351722)
+    
     def test_valid(self):
-        x, y, z = 4449038, 3094664, 3351722
-        p = ECEFPoint(x, y, z)
-        self.assertEqual(p.tup, (x, y, z))
+        p = ECEFPoint(*self.valid)
+        self.assertEqual(p.tup, self.valid)
     
     def test_invalid(self):
         with self.assertRaises(TypeError):
-            ECEFPoint('a', [2], -345)
+            ECEFPoint('a', [2], self.valid.z)
 
 
 class TestPoint2D(unittest.TestCase):
@@ -57,7 +62,28 @@ class TestPoint2D(unittest.TestCase):
 
 
 class TestUTMPoint(unittest.TestCase):
-    pass
+    Point = namedtuple('Point', ['zone', 'east', 'north'])
+    valid = Point(36, 683383.341, 3630033.034)
+    
+    def test_valid(self):
+        p = UTMPoint(*self.valid)
+        self.assertEqual(p.tup, self.valid)
+    
+    def test_invalid(self):
+        with self.assertRaises(TypeError):
+            UTMPoint('a', (3,), self.valid.north)
+    
+    def test_invalid_zone(self):
+        with self.assertRaises(ValueError):
+            UTMPoint(67, self.valid.east, self.valid.north)
+    
+    def test_invalid_east(self):
+        with self.assertRaises(ValueError):
+            UTMPoint(self.valid.zone, 100000, self.valid.north)
+    
+    def test_invalid_north(self):
+        with self.assertRaises(ValueError):
+            UTMPoint(self.valid.zone, self.valid.east, 12000000)
 
 
 if __name__ == '__main__':
